ac355db4f503665b9fc636ca6292ab4ac9dadb38
Smudge <poms17226@gmail.com>
2024-08-30 23:38:58 -0400

    Added bookmark and category

---
+++ app/(tabs)/bookmarks.tsx
@@ -0,0 +1,22 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text,View } from "react-native";
+
+const Page = () => {
+  return (
+    <View style={styles.container}> 
+      <Text>Bookmarks</Text>
+    </View>
+  );
+};
+export default Page;
+
+const styles = StyleSheet.create({
+  container:{
+    flex:1,
+    backgroundColor:'#fff',
+    alignItems:'center',
+    justifyContent:'center'
+
+  },
+
+});
---
+++ app/(tabs)/profile.tsx
@@ -0,0 +1,21 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text,View } from "react-native";
+
+const Page = () => {
+  return (
+    <View style={styles.container}> 
+      <Text>Profile</Text>
+    </View>
+  )
+}
+export default Page;
+const styles = StyleSheet.create({
+  container:{
+    flex:1,
+    backgroundColor:'#fff',
+    alignItems:'center',
+    justifyContent:'center'
+
+  },
+
+});
